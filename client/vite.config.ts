30a3ddb2506b4aade2e84fb14600f3e297266b2b
Lamodot <91544048+lamodots@users.noreply.github.com>
2024-10-06 16:27:39 -0700

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/hsuapp/',
+	base: '/hsuapp/client/',
 	server: {
 		proxy: {
 			api: {
