020b7b93cd942846555b34c5fa655e5b153a173f
lamodots <lamodots@gmail.com>
2024-10-12 14:12:35 -0700

    addded todos

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
-
+//www.youtube.com/watch?v=O3BUHwfHf84&t=5218s
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
